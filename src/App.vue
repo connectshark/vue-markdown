c14ae3f4c50f2731967a78e5565e5d68c33ff537
connectshark <bobhus394@gmail.com>
2021-12-15 14:17:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <router-view />
+</template>
+
+<style>
+html,
+body{
+  margin: 0;
+  height: 100%;
+  font-family: 'Roboto Mono', monospace, 'openhuninn', 'Noto Sans TC', sans-serif;
+  color: #333;
+}
+#app{
+  height: 100vh;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { computed, ref } from 'vue'
+import { marked } from 'marked'
+
+const input = ref('# hello')
+
+const compiledMarkdown = computed(() => {
+  return marked(input.value, { sanitize: true })
+})
+
+const update = (e) => {
+  input.value = e.target.value
+} 
+</script>
+
+<template>
+<main id="editor">
+  <textarea :value="input" @input="update"></textarea>
+  <div v-html="compiledMarkdown"></div>
+</main>
+</template>
+
+
+<style scoped>
+#editor{
+  height: 100%;
+}
+#editor textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: "Monaco", courier, monospace;
+  padding: 20px;
+  height: 100%;
+}
+#editor div{
+  padding: 0 20px;
+}
+code {
+  color: #f66;
+}
+#editor textarea,
+#editor div {
+  display: inline-block;
+  width: 49%;
+  vertical-align: top;
+  box-sizing: border-box;
+}
+</style>
